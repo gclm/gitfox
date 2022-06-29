ce862fb15aec22277301d8a35bd63e0da71b8698
GitHub Action <action@github.com>
2022-06-29 02:52:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/gclm/gitfox/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gclm/gitfox/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gclm.gitfox.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gclm.gitfox.MyBundle
 
 class MyApplicationService {
 
