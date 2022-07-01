0f0045a448d9946e2227feb93dee45680e724c46
gclm <gclmit@163.com>
2022-07-01 17:04:07 +0800

    feat: 新增开源协议生成

---
+++ src/main/java/club/gclmit/plugin/jetbrains/gitfox/config/LicenseTemplateGroupDescriptorFactory.java
@@ -0,0 +1,18 @@
+package club.gclmit.plugin.jetbrains.gitfox.config;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.ide.fileTemplates.FileTemplateGroupDescriptor;
+import com.intellij.ide.fileTemplates.FileTemplateGroupDescriptorFactory;
+
+import club.gclmit.plugin.jetbrains.gitfox.model.Constant;
+
+public class LicenseTemplateGroupDescriptorFactory implements FileTemplateGroupDescriptorFactory {
+    @Override
+    public FileTemplateGroupDescriptor getFileTemplatesDescriptor() {
+        FileTemplateGroupDescriptor group = new FileTemplateGroupDescriptor("Licenses", AllIcons.FileTypes.Text);
+        for (String name : Constant.LICENSE_TEMPLATES) {
+            group.addTemplate(name);
+        }
+        return group;
+    }
+}
